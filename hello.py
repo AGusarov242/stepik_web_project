460022a6936613c6272df5e286cc787e0107ec55
AGusarov242 <55212600+AGusarov242@users.noreply.github.com>
2020-07-25 22:22:26 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def application(env, start_response):
+def application(environ, start_response):
     a =  [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     code = '200 OK'
     headers = [('Content-type', 'text/plain')]
