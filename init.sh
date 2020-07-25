c4ea8c09bed9ff4030ec3e8952c54f23bee15fdf
AGusarov242 <55212600+AGusarov242@users.noreply.github.com>
2020-07-25 22:12:27 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn -c ~/web/etc/gunicorn.conf hello:wsgi_app
+sudo gunicorn -c ~/web/etc/gunicorn.conf hello:application
