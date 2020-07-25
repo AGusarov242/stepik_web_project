31e72a2b9d89b41996ffd392b098527bce144271
AGusarov242 <55212600+AGusarov242@users.noreply.github.com>
2020-07-25 22:12:01 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
-def wsgi_app(environ, start_response):
-	status = '200 OK'
-	headers = [('Content-Type', 'text/plain')]
-	data = environ['QUERY_STRING'].split('&')
-	body = '\n'.join(data).encode('utf-8')
-	start_responce(status, headers)
-	return [body]
+def application(env, start_response):
+    a =  [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    code = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(code, headers)
+    return [ a ]
